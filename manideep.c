189f7c65ab4cc7b5e398bbc62e1322a3e600ded5
Swaroop883 <saiswaroopguntukusaiswaroop@gmail.com>
2025-05-10 13:27:38 +0530

    second commit

---
+++ manideep.c
@@ -0,0 +1,17 @@
+//
+// Created by saisw on 10-05-2025.
+//
+
+//write basic hello world
+
+// manideep.c
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    int a = 10;
+    printf("Hello, World! %d", a);
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ testing.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=20,c=30;
+    int max=0;
+    if(a>b)
+    max=a;
+    else
+    max =b;
+    printf("%d",max);
+    return 0;
+}
